d074026532e6983fc88e969481f1c9c979dc2128
Abdul Richard <Abdul.RIchard@outlook.com>
2026-05-21 06:04:36 +0700

    fix wrong url

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 window.axios = axios
 axios.defaults.withCredentials = false
 // axios.defaults.baseURL = "http://localhost:8081/api"
-let backendUrl = "https://api-abentacos.abdulrichard.site/api"
+let backendUrl = "https://api-ebentacos.abdulrichard.site/api"
 axios.defaults.baseURL = backendUrl
